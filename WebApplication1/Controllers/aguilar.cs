ef659cf8fa7276527ee6e86ffb15795a64be7c37
aguilar <aguilarcanchachij@iestptrujillo.net>
2024-09-30 11:08:44 -0500

    commit hola soy aguilar

---
+++ WebApplication1/Controllers/aguilar.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class aguilar
+    {
+        //HOLA SOY EL CHOLITO ESNAYDER AGUILAR
+    }
+}
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747b39d07275d5bdced271abee5d3ef2f2057c6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a1c2e1ec5294f3055af8c199397cf190843a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
